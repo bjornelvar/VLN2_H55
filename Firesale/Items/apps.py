8d7fa034ffab68600f7d3c328bc8d58ad5466227
HordurThor <hordur20@ru.is>
2022-05-02 15:30:25 +0000

    fix

--- Firesale/Items/apps.py
+++ Firesale/Items/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FiresaleappConfig(AppConfig):
+class ItemsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'firesaleapp'
+    name = 'Items'
