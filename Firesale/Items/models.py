05be4056c742267624b5780fa92002d6df8a8cd2
bjornelvar <bjornet21@ru.is>
2022-05-03 16:21:56 +0000

    test table, ignore

--- Firesale/Items/models.py
+++ Firesale/Items/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# class Items(models.Model):
+#     name = models.CharField(max_length=100)
+#     description = models.TextField()
+#     price = models.FloatField()
+#     created_at = models.DateTimeField(auto_now_add=True) # Bæta við?
+#     sold_status = models.BooleanField(default=False)
+#     condition = models.CharField(max_length=100)
+#     category = models.CharField(max_length=100)
+#     # image = models.ImageField(upload_to='images/')
