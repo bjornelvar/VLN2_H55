5ec754b176f187f04e7cb6369e7b72adcdb6daf3
HordurThor <hordur20@ru.is>
2022-05-04 19:31:47 +0000

    Update views.py

--- Firesale/users/views.py
+++ Firesale/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from users.models import Profiles
 # Create your views here.
 
 def register(request):
@@ -10,4 +11,12 @@ def register(request):
             return redirect('login')
     return render(request, 'users/register.html', {
         'form': UserCreationForm()
+    })
+
+def profile(request):
+    profile = Profiles.objects.filter(user=request.user).first()
+    if request.method == 'POST':
+        print(1)
+    return render(request, 'users/profile.html', {
+        'form': ''
     })
