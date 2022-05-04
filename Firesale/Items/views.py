43bfbd4b2aa899f59dc16bc3953437409479c31e
johannkarlsson <johannk20@ru.is>
2022-05-04 15:15:13 +0000

    prófa display item
    prófa display item

--- Firesale/Items/views.py
+++ Firesale/Items/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from items.models import Items
 
 
 # Create your views here.
 # @login_required(login_url="/%2Fuserslogin")
 def index(response):
+    context = {'items': Items.objects.all().order_by('name')}
     return render(response, 'items/index.html')
