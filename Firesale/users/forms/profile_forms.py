4c0f81218538dccd2bc3d32836b422f792612816
bjornelvar <bjornet21@ru.is>
2022-05-06 15:28:10 +0000

    register html fix og geyma kóða fyrir customregisterform

--- Firesale/users/forms/profile_forms.py
+++ Firesale/users/forms/profile_forms.py
@@ -1,5 +1,20 @@
 from django.forms import ModelForm, widgets
 from users.models import Profiles
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+
+
+# Ætla fá þetta til að virka
+# class CustomRegisterForm(UserCreationForm):
+#     class Meta:
+#         model = Profiles
+#         fields = ['user', 'password1', 'password2']
+#         widgets = {
+#             'username': widgets.TextInput(attrs={'class': 'form-control'}),
+#             'password1': widgets.PasswordInput(attrs={'class': 'form-control'}),
+#             'password2': widgets.PasswordInput(attrs={'class': 'form-control'}),
+#         }
 
 
 class ProfileForm(ModelForm):
