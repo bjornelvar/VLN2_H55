fbe6069b717430a698d9e0b92b5564a295f0dfb6
johannkarlsson <johannk20@ru.is>
2022-05-06 17:12:41 +0000

    reyna láta merga
    reyna láta merga

--- Firesale/Items/views.py
+++ Firesale/Items/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from items.forms.new_listing_form import CreateListingForm
 from items.models import Items
+from items.models import Categories
 
 
 # Create your views here.
@@ -10,6 +11,16 @@ from items.models import Items
 
 def index(response):
     context = {'items': Items.objects.all().order_by('name')}
+    return render(response,   'items/index.html', context)
+
+
+def get_category_list(response):
+    context = {'categories': Categories.objects.all().order_by('name')}
+    return render(response,   'items/category_list.html', context)
+
+
+def get_items_in_category(response):
+    context = {'items_category': Items.objects.filter(category=self.category).order_by('name')}
     return render(response,   'items/index.html', context)
 
 
