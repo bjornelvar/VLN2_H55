eba153391f24d69d73ab092ed6efd0c75a9a5a6b
HordurThor <hordur20@ru.is>
2022-05-06 20:17:28 +0000

    removed profile id, userid er primary key nuna

--- Firesale/users/models.py
+++ Firesale/users/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 
 class Profiles(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     bio = models.CharField(max_length=255, blank=True)
     image = models.ImageField(upload_to='images/', default='images/blank-profile-picture.png')
     rating = models.FloatField(null=True)
