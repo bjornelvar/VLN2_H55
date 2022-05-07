836df9d0c29072ea173c03b659b214de71ef653d
johannkarlsson <johannk20@ru.is>
2022-05-07 19:43:40 +0000

    categories, todolisti og reyna láta search bar á home virka

--- Firesale/home/urls.py
+++ Firesale/home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'home'
+
 urlpatterns = [
-    path('', views.index, name='home-index'), # Sækir úr template>home>home-index.html
+    path('', views.index, name='home-index'),
+    path('search/', views.search_items, name='search-items'),
 ]
--- Firesale/home/views.py
+++ Firesale/home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from items.models import Items
 
 # Create your views here.
 
 def index(response):
     return render(response, 'home/home-index.html')
+
+
+def search_items(request):
+    if request.method == 'POST':
+        search_term = request.POST['search_term']
+        return render(request, 'items/search_items.html', {'search_term': search_term, 'items': Items.objects.filter(name__icontains=search_term)})
+    else:
+        return render(request, 'items/search_items.html', {})
