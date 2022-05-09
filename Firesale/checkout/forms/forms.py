f1fb44d21cc343d6d9874ff9ab2449c878a8ee30
bjornelvar <bjornet21@ru.is>
2022-05-09 21:12:45 +0000

    cool checkout form

--- Firesale/checkout/forms/forms.py
+++ Firesale/checkout/forms/forms.py
@@ -1,35 +1,38 @@
 from django import forms
 from django_countries.fields import CountryField
 
+from checkout.fields import CreditCardField, ExpiryDateField, VerificationValueField
+
 
 class ShippingForm(forms.Form):
-    full_name = forms.CharField()
-    address_1 = forms.CharField()
+    full_name = forms.CharField(required=True)
+    address_1 = forms.CharField(required=True)
     address_2 = forms.CharField(required=False)
-    city = forms.CharField()
-    country = CountryField(blank_label='(select country)').formfield()
-    zip = forms.CharField()
+    city = forms.CharField(required=True)
+    country = CountryField(blank_label='(Select country)').formfield()
+    zip = forms.CharField(required=True)
 
 
 class BillingForm(forms.Form):
-    full_name = forms.CharField()
-    billing_address = forms.CharField()
-    city = forms.CharField()
-    country = CountryField(blank_label='(select country)').formfield()
-    zip = forms.CharField()
-#
-#
-# class PaymentForm(forms.Form):
-#     stripe_id = forms.CharField()
-#     card_number = forms.CharField()
-#     card_cvc = forms.CharField()
-#     card_expiry_date = forms.CharField()
-#     card_name = forms.CharField()
-#     card_address_line_1 = forms.CharField()
-#     card_address_line_2 = forms.CharField(required=False)
-#     card_city = forms.CharField()
-#     card_state = forms.CharField()
-#     card_postal_code = forms.CharField()
-#     card_country = CountryField(blank_label='(select country)').formfield()
+    full_name = forms.CharField(required=True)
+    billing_address = forms.CharField(required=True)
+    city = forms.CharField(required=True)
+    country = CountryField(blank_label='(Select country)').formfield()
+    zip = forms.CharField(required=True)
+
+
+class PaymentForm(forms.Form):
+    name_on_card = forms.CharField(max_length=50,
+                                   required=True,
+                                   widget=forms.TextInput(attrs={'placeholder': 'John Doe'}))
+    card_number = CreditCardField(required=True,
+                                  max_length=16,
+                                  min_length=16,
+                                  widget=forms.TextInput(attrs={'size': '16', 'placeholder': '1234 1234 1234 1234'}))
+    expiry_date = ExpiryDateField(required=True)
+    security_code = VerificationValueField(required=True,
+                                           max_length=3,
+                                           min_length=3,
+                                           widget=forms.TextInput(attrs={'size': '3', 'placeholder': 'CVC'}))
 
 
