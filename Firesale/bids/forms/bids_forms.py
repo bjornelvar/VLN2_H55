61f5e356a1a10e66be71bae7958fe3613940a0a9
bjornelvar <bjornet21@ru.is>
2022-05-10 15:42:43 +0000

    laga item details

--- Firesale/bids/forms/bids_forms.py
+++ Firesale/bids/forms/bids_forms.py
@@ -1,8 +1,19 @@
 from django.forms import ModelForm, widgets
 from bids.models import Bids
 
+
 class CreateBidsForm(ModelForm):
 
     class Meta:
         model = Bids
         exclude = ['id', 'bidder', 'item', 'biddate', 'is_accepted']
+
+
+# class AcceptBidsForm(ModelForm):
+#
+#     class Meta:
+#         model = Bids
+#         exclude = ['id', 'bidder', 'item', 'biddate', 'bidamount']
+#         widgets = {
+#             'is_accepted': widgets.CheckboxInput()
+#         }
