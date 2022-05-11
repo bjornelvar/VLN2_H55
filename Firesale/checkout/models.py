26c8b18a9312bc498c10f35db6c820f10519dd97
HordurThor <hordur20@ru.is>
2022-05-11 22:49:36 +0000

    orders og shippinginfo

--- Firesale/checkout/models.py
+++ Firesale/checkout/models.py
@@ -3,6 +3,15 @@ from users.models import Profiles
 from items.models import Items
 
 
+class ShippingInformation(models.Model):
+    user = models.OneToOneField(Profiles, on_delete=models.CASCADE, primary_key=True)
+    country = models.CharField(max_length=50)
+    zip = models.CharField(max_length=10)
+    city = models.CharField(max_length=50)
+    state_province_region = models.CharField(max_length=100, null=True)
+    address = models.CharField(max_length=150)
+    phone = models.CharField(max_length=15)
+
 
 class Orders(models.Model):
     sender = models.ForeignKey(Profiles, related_name='sender', on_delete=models.CASCADE)
@@ -13,7 +22,4 @@ class Orders(models.Model):
     orderdate = models.DateTimeField(auto_now_add=True)
     sentdate = models.DateTimeField()
     receiveddate = models.DateTimeField()
-
-class ShippingInformation(models.Model):
-    user = models.OneToOneField(Profiles, on_delete=models.CASCADE, primary_key=True)
-    country = models.CharField(max_length=50)
+    shipping_info = models.ForeignKey(ShippingInformation, on_delete=models.CASCADE)
