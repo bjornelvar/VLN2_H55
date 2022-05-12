2b7ce8a99c78d0b05f0edc24f2f8def8a6dd1721
HordurThor <hordur20@ru.is>
2022-05-12 01:50:44 +0000

    checkout migrate og stuff

--- Firesale/users/forms/profile_forms.py
+++ Firesale/users/forms/profile_forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm, widgets
 from users.models import Profiles
+from items.models import Items, ItemImages
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
@@ -37,4 +38,18 @@ class UploadImage(ModelForm):
         exclude = ['id', 'rating', 'user', 'bio']
         widgets = {
             'image': widgets.FileInput(attrs={'class': 'form-control'})
+        }
+
+class EditListing(ModelForm):
+    class Meta:
+        model = Items
+        CONDITION_CHOICES = [('Like New', 'Like New'), ('Very Good', 'Very Good'),
+                             ('Good', 'Good'), ('Fair', 'Fair'), ('Poor', 'Poor')]
+        exclude = ['id', 'listdate', 'seller', 'has_accepted_bid', 'sold']
+        widgets = {
+            'name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'condition': widgets.Select(choices=CONDITION_CHOICES, attrs={'class': 'form-control'}),
+            'description': widgets.Textarea(attrs={'class': 'form-control'}),
+            'category': widgets.Select(attrs={'class': 'form-control'}),
+            'price': widgets.NumberInput(attrs={'class': 'form-control'}),
         }
